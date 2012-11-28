3e341c529b5f8dca69116ed5a7f3549bd2c3d7e4
David Hoelzer <dhoelzer@enclaveforensics.com>
2012-11-28 08:32:46 +0000

    Added netbios.pl

---
+++ Wireless/netbios.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+@names = <STDIN>;
+for $name (@names){
+chomp($name);
+$name =~ s/[^A-P]//;
+@chars = split(//,$name);
+for($x=0;$x<32;$x+=2)
+{
+	$a = $chars[$x];
+	$b = $chars[$x+1];
+
+	$a = ord($a) - ord('A');
+	$b = ord($b) - ord('A');
+	$character = ($a << 4) + $b;
+	print chr($character);
+}
+print "\n";
+}
+
